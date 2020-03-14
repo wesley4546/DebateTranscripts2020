f4a96a1320edfe6610d0ea5ae63fea2faa619740
Wesley G <56905659+wesley4546@users.noreply.github.com>
2020-03-14 16:23:36 -0400

    Annotating/Started on Distribution of Documents

---
+++ R/descriptivestatsondocuments.R
@@ -0,0 +1,144 @@
+library(tidyverse)
+library(tidytext)
+library(ggplot2)
+library(wordcloud)
+library(tidyr)
+
+# Getting Transcripts -----------------------------------------------------
+
+#Gets transcripts
+transripts <-
+  read.csv(
+    here::here("data", "raw", "debate_transcripts_v3_2020-02-26.csv"),
+    stringsAsFactors = FALSE,
+    encoding = "UTF-8"
+  )
+
+
+
+# Getting Bernie's Transcripts --------------------------------------------
+
+#Bernie's Transcripts
+bern_transcript <- 
+  transripts %>% 
+  as_tibble() %>% 
+  filter(speaker == c("Bernie Sanders"))
+
+
+#Creating a document column
+bern_transcript <-
+  bern_transcript %>%
+  mutate(document = (1:nrow(bern_transcript))) %>%
+  as_tibble()
+
+
+#Grouping the transription by debate
+bern_transcript %>% 
+  group_by(document)
+
+
+
+# Cleaning Bernie's Scripts ---------------------------------------------------
+
+#Creating my own stopwords
+my_stop_words <- tibble::tribble(
+  ~word,        ~lexicon,
+  "america",    "custom",
+  "american",   "custom",
+  "people",     "custom",
+  "country",    "custom",
+  "bring",      "custom",
+  "'",          "custom",
+  "don",        "custom",
+  "ve",         "custom",
+  "crosstalk",  "custom"
+)
+
+
+#Adding my own stopwords to the list
+stop_words2 <- stop_words %>% 
+  bind_rows(my_stop_words)
+
+
+#Unnested tokens data
+bern_token <- bern_transcript %>% 
+  unnest_tokens(word, speech, token = "words") %>% 
+  anti_join(stop_words2) %>%  #Stop words
+  arrange(word)
+
+
+#manually removing numbers
+bern_token <- bern_token %>% slice(380:nrow(bern_token)) #Removes the numbers (1:380)
+
+
+
+
+# Descriptive Statistics --------------------------------------------------
+
+#Creates a df with count of words per document as well as speaking time
+words_per_document <-
+  bern_token %>%
+  count(document, speaking_time_seconds)
+summary(words_per_document)
+
+
+#createas lm with n by speaking time
+wcount_by_speakingtime <- lm(n ~ speaking_time_seconds, words_per_document)
+
+
+#Creats a dot plot of words by speaking time
+words_per_time_plot <-
+  ggplot(words_per_document, aes(x = speaking_time_seconds, y = n)) +
+  geom_point() +
+  geom_smooth() +
+  labs(x = "Speaking Time in Seconds",
+       y = "Words",
+       title = "Bernie's Words per Seconds of Speaking Time")
+words_per_time_plot
+
+
+#Creates a distribution plot of the number of words per document
+distribution_of_words <- ggplot(words_per_document, aes(x = n)) +
+  geom_histogram(aes(y = ..density..),      # Histogram with density instead of count on y-axis
+                 binwidth = 1) +
+  geom_density(alpha = .2, fill = "#FF6666") +
+  labs(x = "Number of Words",
+       y = "Density",
+       title = "Distribution of Number of words per Document")
+distribution_of_words
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- R/tfidftopicmodel.R
+++ R/tfidftopicmodel.R
@@ -3,28 +3,42 @@ library(tidytext)
 library(ggplot2)
 library(wordcloud)
 library(tidyr)
-library(microbenchmark)
+
+# Getting Transcripts -----------------------------------------------------
+
+#Gets transcripts
+transripts <-
+  read.csv(
+    here::here("data", "raw", "debate_transcripts_v3_2020-02-26.csv"),
+    stringsAsFactors = FALSE,
+    encoding = "UTF-8"
+  )
 
 
 
-transripts <- read.csv(here::here("Democratic Debate 2020 transcripts","debate_transcripts_v3_2020-02-26.csv"),
-                       stringsAsFactors = FALSE, encoding = "UTF-8")
+# Getting Bernie's Transcripts --------------------------------------------
 
 #Bernie's Transcripts
-bern_trans <- 
+bern_transcript <- 
   transripts %>% 
   as_tibble() %>% 
   filter(speaker == c("Bernie Sanders"))
 
+
 #Creating a document column
-bern_trans <- 
-  bern_trans %>% 
-  mutate(document = (1:nrow(bern_trans))) %>% 
+bern_transcript <-
+  bern_transcript %>%
+  mutate(document = (1:nrow(bern_transcript))) %>%
   as_tibble()
 
+
 #Grouping the transription by debate
-bern_trans %>% 
+bern_transcript %>% 
   group_by(document)
+
+
+
+# Cleaning Bernie's Scripts ---------------------------------------------------
 
 #Creating my own stopwords
 my_stop_words <- tibble::tribble(
@@ -40,40 +54,43 @@ my_stop_words <- tibble::tribble(
   "crosstalk",  "custom"
 )
 
+
 #Adding my own stopwords to the list
 stop_words2 <- stop_words %>% 
   bind_rows(my_stop_words)
 
+
 #Unnested tokens data
-bern_toke <- bern_trans %>% 
+bern_token <- bern_transcript %>% 
   unnest_tokens(word, speech, token = "words") %>% 
   anti_join(stop_words2) %>%  #Stop words
   arrange(word)
 
+
 #manually removing numbers
-bern_toke <- bern_toke %>% slice(380:nrow(bern_toke)) #Removes the numbers (1:380)
+bern_token <- bern_token %>% slice(380:nrow(bern_token)) #Removes the numbers (1:380)
 
 
-############################## Julia Silge's Video mapped to my data
 
+# Julia Silge's Video -----------------------------------------------------
 
 #this is exploring the tf_idf
-bern_toke %>% 
+bern_token %>% 
   count(document,word, sort = TRUE) %>% 
   bind_tf_idf(word,document,n) %>% #creating the tf_idf statistic on tokens
   group_by(document) %>% 
   top_n(10) %>% #selecting the top 10 words
   ungroup %>% 
   mutate(word = reorder(word, tf_idf)) #ordering it by their tf_idf rank
-  
-  # I can't plot 430 documents
-  # ggplot(aes(word, tf_idf, fill = document)) + #plotting it
-  # geom_col(show.legend = FALSE) +
-  # facet_wrap(~document, scales = "free") + #by debate
-  # coord_flip()
-  
+
+# I can't plot 430 documents
+# ggplot(aes(word, tf_idf, fill = document)) + #plotting it
+# geom_col(show.legend = FALSE) +
+# facet_wrap(~document, scales = "free") + #by debate
+# coord_flip()
+
 bern_tf_idf <- 
-  bern_toke %>% 
+  bern_token %>% 
   count(document,word, sort = TRUE) %>% 
   bind_tf_idf(word,document,n)
 
@@ -82,19 +99,23 @@ bern_tf_idf <-
 library(stm)
 library(quanteda)
 
+
 #Creating a dfm
-bern_dfm <- bern_toke %>% 
+bern_dfm <- bern_token %>% 
   count(document,word,sort = TRUE) %>% 
   cast_dfm(document,word,n)
 
-#################################
 
 
-bernie_trans_sparse <- bern_toke %>%
+# Julia Silge's Blog Post -------------------------------------------------
+
+#Creates a cast_spares
+bernie_trans_sparse <- bern_token %>%
   count(document, word) %>%
   cast_sparse(document, word, n)
 
 
+#Parellel computing
 library(furrr)
 plan(multiprocess)
 
@@ -106,49 +127,56 @@ many_models <-
                                           verbose = FALSE)))
 
 
+#Makes a heldout
 heldout <- make.heldout(bernie_trans_sparse)
 
+
+#I have no idea what this is doing
 k_result <- many_models %>%
-  mutate(exclusivity = map(topic_model, exclusivity),
-         semantic_coherence = map(topic_model, semanticCoherence, bernie_trans_sparse),
-         eval_heldout = map(topic_model, eval.heldout, heldout$missing),
-         residual = map(topic_model, checkResiduals, bernie_trans_sparse),
-         bound =  map_dbl(topic_model, function(x) max(x$convergence$bound)),
-         lfact = map_dbl(topic_model, function(x) lfactorial(x$settings$dim$K)),
-         lbound = bound + lfact,
-         iterations = map_dbl(topic_model, function(x) length(x$convergence$bound)))
+  mutate(
+    exclusivity = map(topic_model, exclusivity),
+    semantic_coherence = map(topic_model, semanticCoherence, bernie_trans_sparse),
+    eval_heldout = map(topic_model, eval.heldout, heldout$missing),
+    residual = map(topic_model, checkResiduals, bernie_trans_sparse),
+    bound =  map_dbl(topic_model, function(x)
+      max(x$convergence$bound)),
+    lfact = map_dbl(topic_model, function(x)
+      lfactorial(x$settings$dim$K)),
+    lbound = bound + lfact,
+    iterations = map_dbl(topic_model, function(x)
+      length(x$convergence$bound))
+  )
 
 k_result
 
 
-
+#I also have NO idea what this is doing...yet
 k_result %>%
-  transmute(K,
-            `Lower bound` = lbound,
-            Residuals = map_dbl(residual, "dispersion"),
-            `Semantic coherence` = map_dbl(semantic_coherence, mean),
-            `Held-out likelihood` = map_dbl(eval_heldout, "expected.heldout")) %>%
-  gather(Metric, Value, -K) %>%
+  transmute(
+    K,
+    `Lower bound` = lbound,
+    Residuals = map_dbl(residual, "dispersion"),
+    `Semantic coherence` = map_dbl(semantic_coherence, mean),
+    `Held-out likelihood` = map_dbl(eval_heldout, "expected.heldout")
+  ) %>%
+  gather(Metric, Value,-K) %>%
   ggplot(aes(K, Value, color = Metric)) +
-  geom_line(size = 1.5, alpha = 0.7, show.legend = FALSE) +
-  facet_wrap(~Metric, scales = "free_y") +
-  labs(x = "K (number of topics)",
-       y = NULL,
-       title = "Model diagnostics by number of topics",
-       subtitle = "These diagnostics indicate a good number of topics.")
+  geom_line(size = 1.5,
+            alpha = 0.7,
+            show.legend = FALSE) +
+  facet_wrap( ~ Metric, scales = "free_y") +
+  labs(
+    x = "K (number of topics)",
+    y = NULL,
+    title = "Model diagnostics by number of topics",
+    subtitle = "These diagnostics indicate a good number of topics."
+  )
 
 
 
+# Julia's Video (Continued) -----------------------------------------------
 
-
-
-
-
-
-###################
-
-
-#runnign the topic model
+#running the topic model
 topic_model <- stm(bern_dfm,K = 10 , init.type = "Spectral") #Creating model with 5 topics
 
 
@@ -156,26 +184,26 @@ topic_model <- stm(bern_dfm,K = 10 , init.type = "Spectral") #Creating model wit
 td_beta <- tidy(topic_model)
 
 # visualize the topics
-td_beta %>% 
-  group_by(topic) %>% 
-  top_n(10) %>% 
-  ungroup %>% 
+td_beta %>%
+  group_by(topic) %>%
+  top_n(10) %>%
+  ungroup %>%
   mutate(term = reorder(term, beta)) %>% #ordering it by their beta rank
   ggplot(aes(term, beta, fill = topic)) + #plotting it
   geom_col(show.legend = FALSE) +
-  facet_wrap(~topic, scales = "free") + #by topic
+  facet_wrap( ~ topic, scales = "free") + #by topic
   coord_flip() +
-  labs(
-    title= "k = 10 topics for Bernie's Transcript data"
-  )
+  labs(title = "k = 10 topics for Bernie's Transcript data")
 
 #Measurement of the model using histograms
-td_gamma <- tidy(topic_model, matrix = "gamma",
+td_gamma <- tidy(topic_model,
+                 matrix = "gamma",
                  document_names = rownames(bern_dfm))
 
+#Creates histogram plot
 ggplot(td_gamma, aes(gamma, fill = as.factor(topic))) +
   geom_histogram(show.legend = FALSE) +
-  facet_wrap(~topic, ncol = 5)
+  facet_wrap(~ topic, ncol = 5)
 
 
 
