0dd0be59b2833f115ea7c37efbb2c8a2bc6f1793
Wesley G <56905659+wesley4546@users.noreply.github.com>
2020-03-15 16:37:15 -0400

    Created sourcing

--- R/descriptivestatsondocuments.R
+++ R/descriptivestatsondocuments.R
@@ -1,77 +1,12 @@
 library(tidyverse)
-library(tidytext)
 library(ggplot2)
 library(wordcloud)
 library(tidyr)
 library(patchwork)
 
-# Getting Transcripts -----------------------------------------------------
 
-#Gets transcripts
-transripts <-
-  read.csv(
-    here::here("data", "raw", "debate_transcripts_v3_2020-02-26.csv"),
-    stringsAsFactors = FALSE,
-    encoding = "UTF-8"
-  )
-
-
-
-# Getting Bernie's Transcripts --------------------------------------------
-
-#Bernie's Transcripts
-bern_transcript <- 
-  transripts %>% 
-  as_tibble() %>% 
-  filter(speaker == c("Bernie Sanders"))
-
-
-#Creating a document column
-bern_transcript <-
-  bern_transcript %>%
-  mutate(document = (1:nrow(bern_transcript))) %>%
-  as_tibble()
-
-
-#Grouping the transription by debate
-bern_transcript %>% 
-  group_by(document)
-
-
-
-# Cleaning Bernie's Scripts ---------------------------------------------------
-
-#Creating my own stopwords
-my_stop_words <- tibble::tribble(
-  ~word,        ~lexicon,
-  "america",    "custom",
-  "american",   "custom",
-  "people",     "custom",
-  "country",    "custom",
-  "bring",      "custom",
-  "'",          "custom",
-  "don",        "custom",
-  "ve",         "custom",
-  "crosstalk",  "custom"
-)
-
-
-#Adding my own stopwords to the list
-stop_words2 <- stop_words %>% 
-  bind_rows(my_stop_words)
-
-
-#Unnested tokens data
-bern_token <- bern_transcript %>% 
-  unnest_tokens(word, speech, token = "words") %>% 
-  anti_join(stop_words2) %>%  #Stop words
-  arrange(word)
-
-
-#manually removing numbers
-bern_token <- bern_token %>% slice(380:nrow(bern_token)) #Removes the numbers (1:380)
-
-
+#Gets Bernie's Scripts
+source(here::here("R","berniescripts.R"))
 
 
 # Descriptive Statistics --------------------------------------------------
@@ -121,28 +56,4 @@ boxplot_amt_words
 
 #Creates a plot with all three graphs
 tri_plot <- boxplot_amt_words + distribution_of_words + words_per_time_plot
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+tri_plot
--- R/tfidftopicmodel.R
+++ R/tfidftopicmodel.R
@@ -1,75 +1,12 @@
 library(tidyverse)
 library(tidytext)
 library(ggplot2)
-library(wordcloud)
 library(tidyr)
 
-# Getting Transcripts -----------------------------------------------------
-
-#Gets transcripts
-transripts <-
-  read.csv(
-    here::here("data", "raw", "debate_transcripts_v3_2020-02-26.csv"),
-    stringsAsFactors = FALSE,
-    encoding = "UTF-8"
-  )
 
 
-
-# Getting Bernie's Transcripts --------------------------------------------
-
-#Bernie's Transcripts
-bern_transcript <- 
-  transripts %>% 
-  as_tibble() %>% 
-  filter(speaker == c("Bernie Sanders"))
-
-
-#Creating a document column
-bern_transcript <-
-  bern_transcript %>%
-  mutate(document = (1:nrow(bern_transcript))) %>%
-  as_tibble()
-
-
-#Grouping the transription by debate
-bern_transcript %>% 
-  group_by(document)
-
-
-
-# Cleaning Bernie's Scripts ---------------------------------------------------
-
-#Creating my own stopwords
-my_stop_words <- tibble::tribble(
-  ~word,        ~lexicon,
-  "america",    "custom",
-  "american",   "custom",
-  "people",     "custom",
-  "country",    "custom",
-  "bring",      "custom",
-  "'",          "custom",
-  "don",        "custom",
-  "ve",         "custom",
-  "crosstalk",  "custom"
-)
-
-
-#Adding my own stopwords to the list
-stop_words2 <- stop_words %>% 
-  bind_rows(my_stop_words)
-
-
-#Unnested tokens data
-bern_token <- bern_transcript %>% 
-  unnest_tokens(word, speech, token = "words") %>% 
-  anti_join(stop_words2) %>%  #Stop words
-  arrange(word)
-
-
-#manually removing numbers
-bern_token <- bern_token %>% slice(380:nrow(bern_token)) #Removes the numbers (1:380)
-
+#Gets Bernie's Scripts
+source(here::here("R","berniescripts.R"))
 
 
 # Julia Silge's Video -----------------------------------------------------
@@ -97,7 +34,6 @@ bern_tf_idf <-
 
 #Topic Modeling
 library(stm)
-library(quanteda)
 
 
 #Creating a dfm
