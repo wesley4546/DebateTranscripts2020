0db05c7566102efa4fc249e1cdc8559800bce837
wesley4546 <wesley.gardiner4546@gmail.com>
2020-05-02 17:01:30 -0400

    typos/styling

--- R/candidate_scripts.R
+++ R/candidate_scripts.R
@@ -6,38 +6,37 @@ library(ggplot2)
 library(tidyr)
 
 # These are the scripts in which will be sourced within all files within project.
-# 
+#
 # One issue I have is with the UTf-8 coding and a apostrophe. I just went
-# around that problem the best I could. That means the script wont allow 
-# Norah O'Donnell or Beto O'Rourke. If you have any suggestions in how I can fix this, 
+# around that problem the best I could. That means the script wont allow
+# Norah O'Donnell or Beto O'Rourke. If you have any suggestions in how I can fix this,
 # Please create an issue in GitHub or email me at: wesley.gardiner45456@gmail.com :)
 
 cut_off_time <- 10
 
 # get_Trancsripts function ------------------------------------------------
 
-get_transcripts <- function(){
-  
-  #Gets transcripts
+get_transcripts <- function() {
+
+  # Gets transcripts
   transcripts <-
     read.csv(
       here::here("data", "raw", "debate_transcripts_v3_2020-02-26.csv"),
       stringsAsFactors = FALSE,
       encoding = "UTF-8"
     )
-  
+
   return(transcripts)
-  
 }
 
 # get_candidate_names function --------------------------------------------
 
-get_speaker_names <- function(){
-  
-  #Load's Transcripts
+get_speaker_names <- function() {
+
+  # Load's Transcripts
   transcripts <- get_transcripts()
-  
-  #Retrieves all speaker names
+
+  # Retrieves all speaker names
   names <- unique(transcripts$speaker)
 
   return(names)
@@ -45,44 +44,41 @@ get_speaker_names <- function(){
 
 # get_candidate_transcripts function ------------------------------------------------
 
-get_candidate_transcripts <- function(name, doc_col = FALSE){
-  
-  #get's transcripts
+get_candidate_transcripts <- function(name, doc_col = FALSE) {
+
+  # get's transcripts
   transcripts <- get_transcripts()
-  
-  #Candidate's Name
+
+  # Candidate's Name
   candidate_name <- name
-  
-  #Filters Transcripts to candidate's name
+
+  # Filters Transcripts to candidate's name
   candidate_transcripts <-
     transcripts %>%
     as_tibble() %>%
     filter(speaker == candidate_name)
-  
-  #document arugment if set to TRUE
+
+  # document arugment if set to TRUE
   if (doc_col == TRUE) {
-    #Creating a document column
+    # Creating a document column
     candidate_transcripts <-
       candidate_transcripts %>%
       mutate(document = (1:nrow(candidate_transcripts))) %>%
       as_tibble()
-    
-    #Grouping the transription by debate
+
+    # Grouping the transription by debate
     candidate_transcripts %>%
       group_by(document)
-    
   }
-  
+
   return(candidate_transcripts)
-  
 }
 
 # tokenize_transcripts function ------------------------------------------------------------
 
-tokenize_transcripts <- function(candidate_transcripts, rm_stop = FALSE, rm_num = FALSE){
-  
-  if(rm_stop == TRUE){
-    #Creating my own stopwords
+tokenize_transcripts <- function(candidate_transcripts, rm_stop = FALSE, rm_num = FALSE) {
+  if (rm_stop == TRUE) {
+    # Creating my own stopwords
     custom_stop_words <- tibble::tribble(
       ~word,        ~lexicon,
       "america",    "custom",
@@ -103,46 +99,44 @@ tokenize_transcripts <- function(candidate_transcripts, rm_stop = FALSE, rm_num 
       "time",       "custom",
       "tonight",    "custom",
     )
-    
-    #Adding my own stopwords to the list
+
+    # Adding my own stopwords to the list
     stop_words2 <- stop_words %>%
       bind_rows(custom_stop_words)
-    
-    #Unnested tokens data
+
+    # Unnested tokens data
     candidate_token <- candidate_transcripts %>%
       unnest_tokens(word, speech, token = "words") %>%
-      anti_join(stop_words2) %>%  #Stop words
+      anti_join(stop_words2) %>% # Stop words
       arrange(word)
-    
   }
   else {
-    #Unnested tokens data without stopwords
+    # Unnested tokens data without stopwords
     candidate_token <- candidate_transcripts %>%
       unnest_tokens(word, speech, token = "words") %>%
       arrange(word)
   }
-  
-  
+
+
   if (rm_num == TRUE) {
-    #Removes numbers from word column
+    # Removes numbers from word column
     candidate_token <- candidate_token %>%
       filter(!grepl("[[:digit:]]", word))
   }
-  
-  #Changes date(char) to date(date)
-  candidate_token<-
-    candidate_token %>% 
+
+  # Changes date(char) to date(date)
+  candidate_token <-
+    candidate_token %>%
     mutate(date = mdy(date))
-  
-  #Takes away documents with speaking time below the cut_off_time
+
+  # Takes away documents with speaking time below the cut_off_time
   candidate_token <-
     candidate_token %>%
     filter(speaking_time_seconds > cut_off_time)
-  
-  
-  
+
+
+
   return(candidate_token)
-  
 }
 
 
@@ -158,25 +152,24 @@ tokenize_transcripts <- function(candidate_transcripts, rm_stop = FALSE, rm_num 
 
 
 # candidate_name <- c("Bernie Sanders")
-# 
+#
 # # Gets Candidate's Scripts ------------------------------------------------
-# 
+#
 # source(here::here("R","candidate_scripts.R"))
-# 
+#
 # candidate_transcripts <-
 #   get_candidate_transcripts(candidate_name, doc_col = TRUE)
-# 
+#
 # candidate_token <-
 #   tokenize_transcripts(candidate_transcripts, rm_stop = TRUE, rm_num = TRUE)
 
 
 # Format candidate's name to a filename -----------------------------------
 
-format_filename <- function(candidatesname){
-  
-  #Change's name to lowercase, and no space
-  file_name <- paste(tolower(str_replace_all(string=candidatesname, pattern=" ", repl="")))
-  
+format_filename <- function(candidatesname) {
+
+  # Change's name to lowercase, and no space
+  file_name <- paste(tolower(str_replace_all(string = candidatesname, pattern = " ", repl = "")))
+
   return(file_name)
-  
 }
